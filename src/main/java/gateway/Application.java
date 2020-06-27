acb64dd2b512f56f96a43869f6b1570d1b48ffb8
Matthew Ascone <asconem@gmail.com>
2020-06-27 11:36:16 -0400

    Add fallback RequestMapping

--- src/main/java/gateway/Application.java
+++ src/main/java/gateway/Application.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
@@ -27,6 +31,11 @@ public class Application {
 								.setFallbackUri("forward:/fallback")))
 						.uri("http://httpbin.org:80"))
 				.build();
+	}
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
 	}
 
 }
